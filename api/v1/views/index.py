f2886e320aafa8c30e84c214b7802323b815dcf8
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-05-07 17:40:40 -0300

    fix pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,18 +12,17 @@ from models.user import User
 
 
 @app_views.route("/status")
-def status_url ():
+def status_url():
     """ method that returns a status JSON """
     return jsonify({"status": "OK"})
 
+
 @app_views.route("/stats")
-def stats_url ():
-   """ method that returns a status JSON """
-   return jsonify({
-       "amenities": storage.count(Amenity),
-       "cities": storage.count(City), 
-       "places": storage.count(Place), 
-       "reviews": storage.count(Review), 
-       "states": storage.count(State), 
-       "users": storage.count(User)
-   })
+def stats_url():
+    """ method that returns a status JSON """
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
