a9413d540b5986473cff1360aa00fd108f2b214a
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-05-07 18:11:42 -0300

    task 6

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Api app"""
 from os import getenv
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -18,7 +18,7 @@ def handle_context(code):
 @app.errorhandler(404)
 def not_found(error):
     """ method to handle error 404"""
-    return jsonify({'error': 'Not found'})
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
