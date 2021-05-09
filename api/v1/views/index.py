3f04a1b8e40af2cb9508c41133fa18e764d46da1
Carlos Araque <cararqueu@gmail.com>
2021-05-08 23:06:28 -0300

    fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -11,7 +11,7 @@ from models.state import State
 from models.user import User
 
 
-@app_views.route("/status")
+@app_views.route('/status', strict_slashes=False)
 def status_url():
     """ method that returns a status JSON """
     return jsonify({"status": "OK"})
