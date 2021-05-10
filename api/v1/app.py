13873bebca51b432494d8232175b329b890502d6
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-05-10 09:48:28 -0300

    task 13

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,11 +2,13 @@
 """ Api app"""
 from os import getenv
 from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
